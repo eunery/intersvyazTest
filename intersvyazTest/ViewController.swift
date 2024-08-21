33fa7fc0b6c4c14674d4484f285f773f1cec6ff3
eunery <phisic.cons@yandex.ru>
2024-08-21 13:48:24 +0500

    Initial Commit

---
+++ intersvyazTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  intersvyazTest
+//
+//  Created by Sergei Kulagin on 21.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
